c7b19290ec7967eb657cd01619cf12f689851312
brinst07 <brinst07@gmail.com>
2022-05-17 20:45:43 +0900

    cors 에러 해결

--- src/main/java/com/intellijo/proup/ProUpBackendApplication.java
+++ src/main/java/com/intellijo/proup/ProUpBackendApplication.java
@@ -2,9 +2,15 @@ package com.intellijo.proup;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
-public class ProUpBackendApplication {
+public class ProUpBackendApplication implements WebMvcConfigurer {
+	@Override
+	public void addCorsMappings(CorsRegistry registry) {
+		registry.addMapping("/api/**").allowedOrigins("*");
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(ProUpBackendApplication.class, args);
